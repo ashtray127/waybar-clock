86dd68d832e6c6718496130836a83c48f6c95bee
ashtray127 <github@doxle.net>
2024-11-26 22:37:53 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+<h1>waybar-clock</h1>
+
+A small clock module for waybar which uses [nerd font](https://www.nerdfonts.com/) symbols and updates the hour hand of the clock :D
---
+++ main.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+
+from datetime import datetime
+import json
+
+clocks = [
+    "", # 1
+    "󱑌", # 2
+    "󱑍", # 3
+    "󱑎", # 4
+    "󱑏", # 5
+    "󱑐", # 6
+    "󱑑", # 7
+    "󱑒", # 8
+    "󱑓", # 9
+    "󱑔", # 10
+    "󱑕", # 11
+    "󱑖", # 12
+]
+
+current_time = datetime.now()
+
+current_hour = current_time.strftime("%I")
+
+formatted_time = current_time.strftime("%I:%M")
+
+text =  clocks[int(current_hour)] + " " + formatted_time
+
+data = {
+    "text": text,
+    "tooltip": "",
+    "class":current_hour
+}
+
+print(json.dumps(data))
